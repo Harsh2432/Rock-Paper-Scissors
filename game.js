3c8836036c89a8851db9d8d5493fc8c2e659855b
Harsh2432 <jadhavharsh243@gmail.com>
2022-04-04 19:39:30 +0530

    Edit game.js

--- game.js
+++ game.js
@@ -1,30 +1,34 @@
 function game() {
-    function welcome() {
-        alert("Welcome Player!");
-    }
-    function playername() {
-        let name = prompt("Can you tell me your name?", "");
-        function greetings() {
-            alert(`Hello ${name}!`);
-            let desicion = confirm("Are you feeling bored?");
-            if (decision) {
-                alert("So let's play a game!");
-                let play = confirm("You ready?");
-                if (play) {
-                    function playgame() {
-                        alert(`We are going to play Rock-Paper-Scissors ${name}`);
-                        alert("I hope you are excited!");
-                        alert("Let's Start!");
-                    }
-                }
-                else {
-                    alert("Ok, then! We can play some another time!");
-                }
-            }
-            else {
-                alert("Seems like you are having a nice time, Enjoy!");
-            }
+    welcome();
+    playername();
+    greetings();
+}
+function welcome() {
+    alert("Welcome Player!");
+}
+function playername() {
+    let name = prompt("Can you tell me your name?", "");
+    alert(`Hello ${name}!`);
+}
+function greetings() {
+    let desicion = confirm("Are you feeling bored?");
+    if (desicion) {
+        alert("So let's play a game!");
+        let play = confirm("You ready?");
+        if (play) {
+            playgame();
+        }
+        else {
+            alert("Ok, then! We can play some another time!");
         }
     }
+    else {
+        alert("Seems like you are having a nice time, Enjoy!");
+    }
+}
+function playgame() {
+    alert("We are going to play Rock-Paper-Scissors!");
+    alert("I hope you are excited!");
+    alert("Let's Start!");
 }
 game();
