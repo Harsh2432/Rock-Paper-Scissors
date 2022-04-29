c95f20cef2e18fc2d52eaa94709036ed19a331ef
Harsh2432 <jadhavharsh243@gmail.com>
2022-04-29 19:54:51 +0530

    Edit Rock-Paper-Scissor.js

--- Rock-Paper-Scissor.js
+++ Rock-Paper-Scissor.js
@@ -30,11 +30,16 @@ let playerPoint = 0;
 
 let computerPoint = 0;
 
+userPoints.textContent = playerPoint;
+
+compPoints.textContent = computerPoint;
+
 const welcome = document.createElement("div");
 welcome.classList.add("welcome");
 welcome.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px;";
 
 let welcomePlayer = function () {
+
     welcome.textContent = "Welcome Player!";
 
     container.insertBefore(welcome, inner);
@@ -46,23 +51,23 @@ const loadingDec = document.createElement("div");
 loadingDec.classList.add("loadingDec");
 loadingDec.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px; gap: 10px;";
 
+const load = document.createElement("div");
+load.classList.add("load");
+load.textContent = "LOADING";
+load.style.cssText = "display: flex; flex: 2; color: white; background-color: coral; justify-content: center; align-items: center; font-family: monospace; padding: 10px; font-size: 18px; font-weight: bold; border-radius: 10px;";
+
+loadingDec.appendChild(load);
+
+const loader = document.createElement("div");
+loader.classList.add("loader");
+
+loadingDec.appendChild(loader);
+
 let loading = function () {
 
     container.replaceChild(loadingDec, welcome);
 
-    const load = document.createElement("div");
-    load.classList.add("load");
-    load.textContent = "Loading";
-    load.style.cssText = "display: flex; flex: 2; color: white; background-color: coral; justify-content: center; align-items: center; font-family: monospace; padding: 10px; font-size: 18px; font-weight: bold; border-radius: 10px;";
-
-    loadingDec.appendChild(load);
-
-    const loader = document.createElement("div");
-    loader.classList.add("loader");
-
-    loadingDec.appendChild(loader);
-
-    setTimeout(playerName, 3000);
+    setTimeout(playerName, 2000);
 };
 
 const player = document.createElement("div");
@@ -70,8 +75,6 @@ player.classList.add("player");
 player.style.cssText = "display: flex; flex-direction:column; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px; gap: 10px;";
 
 let playerName = function () {
-
-    container.replaceChild(player, loadingDec);
 
     const question = document.createElement("div");
     question.classList.add("question");
@@ -96,14 +99,18 @@ let playerName = function () {
     sumbit.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; background-color: coral; font-family: monospace; font-size: 18px; font-weight: bold; padding: 10px; border-radius: 10px;";
 
     sumbit.addEventListener("click", () => {
+
         pName = document.getElementById("inputName").value;
         user.textContent = pName.toUpperCase();
+
         greetings();
     });
 
     innerName.appendChild(sumbit);
 
     player.appendChild(innerName);
+
+    container.replaceChild(player, loadingDec);
 };
 
 const greeting = document.createElement("div");
@@ -115,6 +122,7 @@ let greetings = function () {
     container.replaceChild(greeting, player);
 
     greeting.textContent = `Hello ${pName}!`;
+
     rounds();
 };
 
@@ -124,37 +132,44 @@ roundDec.style.cssText = "display: flex; flex: 1; justify-content: center; align
 
 let rounds = function () {
 
+    document.querySelector(".rockButton").disabled = false;
+
+    document.querySelector(".paperButton").disabled = false;
+
+    document.querySelector(".scissorButton").disabled = false;
+
     container.insertBefore(roundDec, inner);
 
     roundDec.textContent = `Round ${round} Begins!`;
+
     pSelection();
 };
-
-userPoints.textContent = playerPoint;
-
-compPoints.textContent = computerPoint;
 
 const playerSelect = document.createElement("div");
 playerSelect.classList.add("playerSelect");
 playerSelect.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px;";
 
 let pSelection = function () {
+
     playerSelect.textContent = `So what do you choose ${pName}?`;
 
     container.insertBefore(playerSelect, options);
 
     rockButton.addEventListener("click", () => {
         playerchoice = "Rock";
+
         userSelect();
     });
 
     paperButton.addEventListener("click", () => {
         playerchoice = "Paper";
+
         userSelect();
     });
 
     scissorButton.addEventListener("click", () => {
         playerchoice = "Scissor";
+
         userSelect();
     });
 };
@@ -165,7 +180,9 @@ userSelection.style.cssText = "display: flex; flex: 1; justify-content: center; 
 
 let userSelect = function () {
     greeting.remove();
+
     playerSelect.remove();
+
     userSelection.textContent = `${playerchoice} is been selected by ${pName}!`;
 
     container.insertBefore(userSelection, options);
@@ -174,6 +191,7 @@ let userSelect = function () {
 };
 
 let choices = ["Rock", "Paper", "Scissor"];
+
 computerchoice = choices[Math.floor(Math.random() * choices.length)];
 
 const computerSelection = document.createElement("div");
@@ -197,103 +215,120 @@ scorePlus.classList.add("scorePlus");
 scorePlus.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px;";
 
 let results = function () {
+    document.querySelector(".rockButton").disabled = true;
+
+    document.querySelector(".paperButton").disabled = true;
+
+    document.querySelector(".scissorButton").disabled = true;
+
     if (playerchoice == "Rock" && computerchoice == "Scissor") {
         result.textContent = `${playerchoice} smashes ${computerchoice}!`;
         playerPoint += 1;
+        if (playerPoint > round && round > 1) {
+            playerPoint = playerPoint - round + 1;
+        }
+        else if (playerPoint == round && round > 1) {
+            playerPoint = playerPoint - round + 1
+        }
         scorePlus.textContent = `${pName} scores 1 point!`;
     }
+
     else if (playerchoice == "Rock" && computerchoice == "Paper") {
         result.textContent = `${computerchoice} covers ${playerchoice}!`;
         computerPoint += 1;
+        if (computerPoint > round && round > 1) {
+            computerPoint = computerPoint - round + 1;
+        }
+        else if (computerPoint == round && round > 1) {
+            computerPoint = computerPoint - round + 1;
+        }
         scorePlus.textContent = `${comp} scores 1 point!`;
     }
+
     else if (playerchoice == "Paper" && computerchoice == "Rock") {
         result.textContent = `${playerchoice} covers ${computerchoice}!`;
         playerPoint += 1;
+        if (playerPoint > round && round > 1) {
+            playerPoint = playerPoint - round + 1;
+        }
+        else if (playerPoint == round && round > 1) {
+            playerPoint = playerPoint - round + 1
+        }
         scorePlus.textContent = `${pName} scores 1 point!`;
     }
+
     else if (playerchoice == "Paper" && computerchoice == "Scissor") {
         result.textContent = `${computerchoice} cuts ${playerchoice}!`;
         computerPoint += 1;
+        if (computerPoint > round && round > 1) {
+            computerPoint = computerPoint - round + 1;
+        }
+        else if (computerPoint == round && round > 1) {
+            computerPoint = computerPoint - round + 1;
+        }
         scorePlus.textContent = `${comp} scores 1 point!`;
     }
+
     else if (playerchoice == "Scissor" && computerchoice == "Rock") {
         result.textContent = `${computerchoice} smashes ${playerchoice}!`;
         computerPoint += 1;
+        if (computerPoint > round && round > 1) {
+            computerPoint = computerPoint - round + 1;
+        }
+        else if (computerPoint == round && round > 1) {
+            computerPoint = computerPoint - round + 1;
+        }
         scorePlus.textContent = `${comp} scores 1 point!`;
     }
+
     else if (playerchoice == "Scissor" && computerchoice == "Paper") {
         result.textContent = `${playerchoice} cuts ${computerchoice}!`;
         playerPoint += 1;
+        if (playerPoint > round && round > 1) {
+            playerPoint = playerPoint - round + 1;
+        }
+        else if (playerPoint == round && round > 1) {
+            playerPoint = playerPoint - round + 1
+        }
         scorePlus.textContent = `${pName} scores 1 point!`;
     }
+
     else {
         result.textContent = "It\'s a Tie!";
+        playerPoint += 0;
+        computerPoint += 0;
         scorePlus.textContent = "No one scores any point!";
     }
 
     userPoints.textContent = playerPoint;
+
     compPoints.textContent = computerPoint;
 
     container.appendChild(result);
 
     container.appendChild(scorePlus);
 
-    setTimeout(pageReload, 5000);
+    setTimeout(pageReload, 3000);
 };
 
 let pageReload = function () {
+
     userSelection.remove();
+
     computerSelection.remove();
+
     result.remove();
+
     scorePlus.remove();
-    loadingFunction();
+    
+    loadFunc();
 };
 
-const loadDec = document.createElement("div");
-loadDec.classList.add("loadDec");
-loadDec.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px; gap: 10px;";
+let loadFunc = function () {
 
-let loadingFunction = function () {
+    container.replaceChild(loadingDec, roundDec);
 
-    const load = document.createElement("div");
-    load.classList.add("load");
-    load.textContent = "Loading";
-    load.style.cssText = "display: flex; flex: 2; color: white; background-color: coral; justify-content: center; align-items: center; font-family: monospace; padding: 10px; font-size: 18px; font-weight: bold; border-radius: 10px;";
-
-    loadDec.appendChild(load);
-
-    const loader = document.createElement("div");
-    loader.classList.add("loader");
-
-    loadDec.appendChild(loader);
-
-    container.replaceChild(loadDec, roundDec);
-
-    setTimeout(roundAgain, 3000);
-};
-
-const loadResult = document.createElement("div");
-loadResult.classList.add("loadResult");
-loadResult.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px; gap: 10px;";
-
-let loadingFunc = function () {
-
-    const load = document.createElement("div");
-    load.classList.add("load");
-    load.textContent = "Loading";
-    load.style.cssText = "display: flex; flex: 2; color: white; background-color: coral; justify-content: center; align-items: center; font-family: monospace; padding: 10px; font-size: 18px; font-weight: bold; border-radius: 10px;";
-
-    loadResult.appendChild(load);
-
-    const loader = document.createElement("div");
-    loader.classList.add("loader");
-
-    loadResult.appendChild(loader);
-
-    container.replaceChild(loadResult, sayBye);
-
-    setTimeout(endResult, 3000);
+    setTimeout(roundAgain, 2000);
 };
 
 const sayBye = document.createElement("div");
@@ -301,7 +336,6 @@ sayBye.classList.add("sayBye");
 sayBye.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px;";
 
 let roundAgain = function () {
-    clearTimeout(setTimeout(pageReload, 5000));
 
     const anotherRound = document.createElement("div");
     anotherRound.classList.add("anotherRound");
@@ -324,8 +358,11 @@ let roundAgain = function () {
     yesButton.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; background-color: coral; font-family: monospace; font-size: 18px; font-weight: bold; padding: 10px; border-radius: 10px;";
 
     yesButton.addEventListener("click", () => {
+
         anotherRound.remove();
+
         round += 1;
+
         rounds();
     });
 
@@ -337,28 +374,38 @@ let roundAgain = function () {
     noButton.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; background-color: coral; font-family: monospace; font-size: 18px; font-weight: bold; padding: 10px; border-radius: 10px;";
 
     noButton.addEventListener("click", () => {
+
         anotherRound.remove();
+
         sayBye.textContent = `Ok ${pName}, we will play some another time! Bye!`;
 
         container.insertBefore(sayBye, inner);
 
-        setTimeout(loadingFunc, 3000);
+        setTimeout(loadFunction, 3000);
     });
 
     playerDec.appendChild(noButton);
 
     anotherRound.appendChild(playerDec);
 
-    container.replaceChild(anotherRound, loadDec);
+    container.replaceChild(anotherRound, loadingDec);
+};
+
+let loadFunction = function () {
+
+    container.replaceChild(loadingDec, sayBye);
+
+    setTimeout(endResult, 2000);
 };
 
 let endResult = function () {
+
     const disResult = document.createElement("div");
     disResult.classList.add("disResult");
     disResult.textContent = "Time to display end Result! ";
     disResult.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px;";
 
-    container.replaceChild(disResult, loadResult);
+    container.replaceChild(disResult, loadingDec);
 
     numRounds();
 };
@@ -381,11 +428,13 @@ morePoints.style.cssText = "display: flex; flex: 1; justify-content: center; ali
 
 let disPoint = function () {
     if (playerPoint > computerPoint) {
-        morePoints.textContent = `${pName} scores ${playerPoint - computerPoint} more points than Computer!`;
+        morePoints.textContent = `${pName} scores ${playerPoint - computerPoint} points more than Computer!`;
     }
+
     else if (playerPoint < computerPoint) {
-        morePoints.textContent = `Computer scores ${computerPoint - playerPoint} more than ${pName}!`;
+        morePoints.textContent = `Computer scores ${computerPoint - playerPoint} points more than ${pName}!`;
     }
+
     else {
         morePoints.textContent = `${pName} and Computer score equal points!`;
     }
@@ -400,23 +449,27 @@ winner.classList.add("winner");
 winner.style.cssText = "display: flex; flex: 1; justify-content: center; align-items: center; color: white; border: 2px solid white; border-radius: 10px; padding: 10px; font-family: monospace; font-size: 18px; width: 515px;";
 
 let webReload = function () {
+
     location.reload();
 }
 
 let disWinner = function () {
+
     if (playerPoint > computerPoint) {
         winner.textContent = `${pName} Wins! Computer Lose!`;
     }
+
     else if (playerPoint < computerPoint) {
         winner.textContent = `Computer Wins! ${pName} Lose!`;
     }
+
     else {
         winner.textContent = `It\'s a Tie!`;
     }
 
     container.appendChild(winner);
 
-    setTimeout(webReload, 5000);
+    setTimeout(webReload, 10000);
 };
 
 welcomePlayer();
